069adb5e3a469935d9403dbc44f4f00e089a3a76
Anh Tuan <tuanl799@gmail.com>
2025-03-19 23:28:48 +0700

    feat(RTOS_TASK): Add BLE Task

---
+++ src/tasks/ble_task.cpp
@@ -0,0 +1,88 @@
+/**
+ * @file       ble_task.cpp
+ * @license    This library is released under the MIT License.
+ * @version    0.1.0
+ * @date       2025-02-01
+ * @author     Tuan Nguyen
+ *
+ * @brief      Source file for BLE Task
+ *
+ */
+
+/* Includes ----------------------------------------------------------- */
+#include "ble_task.h"
+#include "globals.h"
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+
+/* Private defines ---------------------------------------------------- */
+
+/* Private enumerate/structure ---------------------------------------- */
+
+/* Private macros ----------------------------------------------------- */
+
+/* Public variables --------------------------------------------------- */
+
+/* Private variables -------------------------------------------------- */
+BLECharacteristic *ssidCharacteristic;
+BLECharacteristic *passwordCharacteristic;
+
+/* Class method definitions ------------------------------------------- */
+
+class SSIDCallback : public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *characteristic) override
+  {
+    std::string value = characteristic->getValue();
+    if (value.length() < sizeof(ssid))
+    {
+      memset(ssid, 0, sizeof(ssid));
+      memcpy(ssid, value.c_str(), value.length());
+      // ssid[value.length()] = '\0';
+    }
+  }
+};
+
+class PasswordCallback : public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *characteristic) override
+  {
+    std::string value = characteristic->getValue();
+    if (value.length() < sizeof(password))
+    {
+      memset(password, 0, sizeof(password));
+      memcpy(password, value.c_str(), value.length());
+    }
+  }
+};
+
+/* Task definitions ------------------------------------------- */
+
+void bleTask(void *pvParameters)
+{
+  BLEDevice::init("XIAO ESP32-S3");
+  BLEServer  *pServer  = BLEDevice::createServer();
+  BLEService *pService = pServer->createService("c3251bfb-5e94-4a01-93e7-6cb22b839cac");
+
+  ssidCharacteristic =
+  pService->createCharacteristic("b6c3dda3-c00b-4fa0-a34f-ca91de83a2e4", BLECharacteristic::PROPERTY_WRITE);
+  passwordCharacteristic =
+  pService->createCharacteristic("8f938de4-56db-45df-9333-912e290efed9", BLECharacteristic::PROPERTY_WRITE);
+
+  // Attach callbacks
+  ssidCharacteristic->setCallbacks(new SSIDCallback());
+  passwordCharacteristic->setCallbacks(new PasswordCallback());
+  pService->start();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID("c3251bfb-5e94-4a01-93e7-6cb22b839cac");
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  vTaskDelete(NULL);
+}
+
+void bleSetup() { xTaskCreate(bleTask, "BLETask", 4096, NULL, 1, NULL); }
+
+/* End of file -------------------------------------------------------- */
