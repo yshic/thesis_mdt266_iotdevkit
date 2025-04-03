67e8f136523527038cd7b02e8d9e4962a639b375
Anh Tuan <tuanl799@gmail.com>
2025-04-03 15:10:51 +0700

    update(RTOS): Add SHT40 Task; Add BMP280 Task; Add Unit ENV IV Task; Add AC Measure Module Task; Add preprocessing directive for switching between sensor modules

--- src/tasks/sensors_task.cpp
+++ src/tasks/sensors_task.cpp
@@ -22,10 +22,9 @@
 /* Public variables --------------------------------------------------- */
 
 /* Private variables -------------------------------------------------- */
-float temperature;
-float humidity;
 
 /* Task definitions ------------------------------------------- */
+#ifdef DHT20_MODULE
 void dht20Task(void *pvParameters)
 {
   for (;;)
@@ -34,6 +33,67 @@ void dht20Task(void *pvParameters)
     vTaskDelay(DELAY_DHT20 / portTICK_PERIOD_MS);
   }
 }
+
+void dht20Setup()
+{
+  dht20.begin();
+  xTaskCreate(dht20Task, "DHT 20 Task", 8192, NULL, 1, NULL);
+}
+#endif // DHT20_MODULE
+
+#ifdef SHT4X_MODULE
+void sht40Task(void *pvParameters)
+{
+  for (;;)
+  {
+    sht40.update();
+    vTaskDelay(DELAY_SHT4X / portTICK_PERIOD_MS);
+  }
+}
+void sht40Setup()
+{
+  sht40.begin();
+  sht40.setHeater(SHT4X_NO_HEATER);
+  sht40.setPrecision(SHT4X_HIGH_PRECISION);
+  xTaskCreate(sht40Task, "SHT40 Task", 8192, NULL, 1, NULL);
+}
+#endif // SHT4X_MODULE
+
+#ifdef BMP280_MODULE
+void bmp280Task(void *pvParameters)
+{
+  for (;;)
+  {
+    bmp280.update();
+    vTaskDelay(DELAY_BMP280 / portTICK_PERIOD_MS);
+  }
+}
+
+void bmp280Setup()
+{
+  bmp280.begin();
+  bmp280.setSampling(MODE_NORMAL,     /* Operating Mode. */
+                     SAMPLING_X2,     /* Temp. oversampling */
+                     SAMPLING_X16,    /* Pressure oversampling */
+                     FILTER_X16,      /* Filtering. */
+                     STANDBY_MS_500); /* Standby time. */
+  xTaskCreate(bmp280Task, "BMP280 Task", 8192, NULL, 1, NULL);
+}
+#endif // BMP280_MODULE
+
+#if defined(SHT4X_MODULE) && defined(BMP280_MODULE)
+void unitENVIVSetup()
+{
+  sht40Setup();
+  bmp280Setup();
+}
+#endif // defined(SHT4X_MODULE) && defined(BMP280_MODULE)
+
+#ifdef AC_MEASURE_MODULE
+void acMeasureSetup() { acMeasure.begin(); }
+#endif // AC_MEASURE_MODULE
+
+#ifdef LIGHT_SENSOR_MODULE
 void lightSensorTask(void *pvParameters)
 {
   for (;;)
@@ -42,6 +102,11 @@ void lightSensorTask(void *pvParameters)
     vTaskDelay(DELAY_LIGHT_SENSOR / portTICK_PERIOD_MS);
   }
 }
+
+void lightSensorSetup() { xTaskCreate(lightSensorTask, "Light Sensor Task", 4096, NULL, 2, NULL); }
+#endif // LIGHT_SENSOR_MODULE
+
+#ifdef ULTRASONIC_MODULE
 void ultrasonicTask(void *pvParameters)
 {
   for (;;)
@@ -52,6 +117,10 @@ void ultrasonicTask(void *pvParameters)
     vTaskDelay(DELAY_ULTRASONIC / portTICK_PERIOD_MS);
   }
 }
+void ultrasonicSetup() { xTaskCreate(ultrasonicTask, "Ultrasonic Sensor Task", 4096, NULL, 2, NULL); }
+#endif // ULTRASONIC_MODULE
+
+#ifdef PIR_MODULE
 void pirSensorTask(void *pvParameters)
 {
   for (;;)
@@ -60,7 +129,10 @@ void pirSensorTask(void *pvParameters)
     vTaskDelay(DELAY_PIRSENSOR / portTICK_PERIOD_MS);
   }
 }
+void pirSensorSetup() { xTaskCreate(pirSensorTask, "PIR Sensor Task", 4096, NULL, 2, NULL); }
+#endif // PIR_MODULE
 
+#ifdef SOIL_MOISTURE_MODULE
 void soilMoistureTask(void *pvParameters)
 {
   for (;;)
@@ -69,19 +141,7 @@ void soilMoistureTask(void *pvParameters)
     vTaskDelay(DELAY_MOISTURE / portTICK_PERIOD_MS);
   }
 }
-
-void dht20Setup()
-{
-  dht20.begin();
-  xTaskCreate(dht20Task, "DHT 20 Task", 1024, NULL, 1, NULL);
-}
-
-void lightSensorSetup() { xTaskCreate(lightSensorTask, "Light Sensor Task", 1024, NULL, 1, NULL); }
-
-void ultrasonicSetup() { xTaskCreate(ultrasonicTask, "Ultrasonic Sensor Task", 1024, NULL, 2, NULL); }
-
-void pirSensorSetup() { xTaskCreate(pirSensorTask, "PIR Sensor Task", 1024, NULL, 2, NULL); }
-
-void soilMoistureSetup() { xTaskCreate(soilMoistureTask, "Soil Moisture Task", 1024, NULL, 1, NULL); }
+void soilMoistureSetup() { xTaskCreate(soilMoistureTask, "Soil Moisture Task", 4096, NULL, 1, NULL); }
+#endif // SOIL_MOISTURE_MODULE
 
 /* End of file -------------------------------------------------------- */
--- src/tasks/sensors_task.h
+++ src/tasks/sensors_task.h
@@ -22,6 +22,8 @@
 
   /* Public defines ----------------------------------------------------- */
   #define DELAY_DHT20        15000
+  #define DELAY_SHT4X        15000
+  #define DELAY_BMP280       15000
   #define DELAY_LIGHT_SENSOR 1000
   #define DELAY_ULTRASONIC   1000
   #define DELAY_PIRSENSOR    1000
@@ -35,11 +37,18 @@
 
 /* Funtions Declaration -------------------------------------------------- */
 void dht20Task(void *pvParameters);
+void sht40Task(void *pvParameters);
+void bmp280Task(void *pvParameters);
+void acMeasureTask(void *pvParameters);
 void lightSensorTask(void *pvParameters);
 void ultraSonicTask(void *pvParameters);
 void pirSensorTask(void *pvParameters);
 
 void dht20Setup();
+void sht40Setup();
+void bmp280Setup();
+void unitENVIVSetup();
+void acMeasureSetup();
 void lightSensorSetup();
 void ultrasonicSetup();
 void pirSensorSetup();
