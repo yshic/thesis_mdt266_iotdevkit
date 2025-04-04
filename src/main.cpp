114a2b2e16113afb94d893049c17fe5e26d73427
Anh Tuan <tuanl799@gmail.com>
2025-04-04 16:21:01 +0700

    update: Add preprocessing directive for switching modules

--- src/main.cpp
+++ src/main.cpp
@@ -24,29 +24,58 @@ void setup()
   Serial.begin(9600);
   Wire.begin();
   bspGpioPinMode(LED_BUILTIN, OUTPUT);
-  bspGpioDigitalWrite(LED_BUILTIN, HIGH);
 
 #ifdef MAIN
-  lcd.begin(&Wire);
-  lcd.display();
-  lcd.backlight();
-  lcd.clear();
-
+  #ifndef ENTERPRISE_WIFI
   bleSetup();
-  wifiSetup();
+  #endif
 
+  #ifdef WIFI_MODULE
+  wifiSetup();
+  #endif
+
+  #ifdef DHT20_MODULE
   dht20Setup();
+  #endif
+
+  #ifdef SHT4X_MODULE
+  sht40Setup();
+  #endif
+
+  #ifdef LIGHT_SENSOR_MODULE
   lightSensorSetup();
+  #endif
+
+  #ifdef ULTRASONIC_MODULE
   ultrasonicSetup();
+  #endif
+
+  #ifdef PIR_MODULE
   pirSensorSetup();
+  #endif
+
+  #ifdef SOIL_MOISTURE_MODULE
   soilMoistureSetup();
-#endif
+  #endif
+
+  #ifdef LCD_MODULE
+  lcdSetup();
+  #endif
+
+  #ifdef UNIT_4_RELAY_MODULE
+  unit4RelaySetup();
+  #endif
+
+  #ifdef IOT_SERVER_MODULE
+  iotServerSetup();
+  #endif
+#endif // MAIN
 
 #ifdef DEBUG_I2C
   while (!Serial)
     ; // Wait for Serial to be ready
   scanI2CDevices();
-#endif
+#endif // DEBUG_I2C
 }
 
 void loop()
