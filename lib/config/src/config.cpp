2826a47015346a9abd245549def6910a9b2483a5
Anh Tuan <tuanl799@gmail.com>
2025-04-04 16:49:52 +0700

    feat(CFG): Add global config

---
+++ lib/config/src/config.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file       config.cpp
+ * @license    This library is released under the MIT License.
+ * @version    0.1.0
+ * @date       2025-03-05
+ * @author     Tuan Nguyen
+ *
+ * @brief      Source file for Global configuration
+ *
+ */
+
+/* Includes ----------------------------------------------------------- */
+#include "config.h"
+
+/* Private defines ---------------------------------------------------- */
+
+/* Private enumerate/structure ---------------------------------------- */
+
+/* Private macros ----------------------------------------------------- */
+
+/* Public variables --------------------------------------------------- */
+
+/* Private variables -------------------------------------------------- */
+
+/* Class method definitions ------------------------------------------- */
+
+/* Private function prototypes ---------------------------------------- */
+
+/* End of file -------------------------------------------------------- */
