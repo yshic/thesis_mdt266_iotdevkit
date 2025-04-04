5518aa83fe7dc134e0257eb61e6b144f6005a9b9
Anh Tuan <tuanl799@gmail.com>
2025-04-04 16:54:14 +0700

    feat(BSP): Init bsp_rs485

---
+++ lib/bsp/bsp_rs485.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file       bsp_rs485.cpp
+ * @license    This library is released under the MIT License.
+ * @version    0.1.0
+ * @date       2025-03-29
+ * @author     Tuan Nguyen
+ *
+ * @brief      Source file for bsp_rs485.cpp library
+ *
+ */
+
+/* Includes ----------------------------------------------------------- */
+
+/* Private defines ---------------------------------------------------- */
+
+/* Private enumerate/structure ---------------------------------------- */
+
+/* Private macros ----------------------------------------------------- */
+
+/* Public variables --------------------------------------------------- */
+
+/* Private variables -------------------------------------------------- */
+
+/* Class method definitions-------------------------------------------- */
+
+/* Private function prototypes ---------------------------------------- */
+
+/* End of file -------------------------------------------------------- */
---
+++ lib/bsp/bsp_rs485.h
@@ -0,0 +1,39 @@
+/**
+ * @file       bsp_rs485.h
+ * @license    This library is released under the MIT License.
+ * @version    0.1.0
+ * @date       2025-03-29
+ * @author     Tuan Nguyen
+ *
+ * @brief      Header file for BSP_RS485 library
+ */
+
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef BSP_RS485_H
+#define BSP_RS485_H
+
+/* Includes ----------------------------------------------------------- */
+#if ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+/* Public defines ----------------------------------------------------- */
+#define BSP_RS485_LIB_VERSION (F("0.1.0"))
+
+/* Public enumerate/structure ----------------------------------------- */
+
+/* Public macros ------------------------------------------------------ */
+
+/* Public variables --------------------------------------------------- */
+
+/* Class Declaration -------------------------------------------------- */
+class BspRs485 {
+  public:
+  private:
+};
+
+#endif // BSP_RS485_H
+
+/* End of file -------------------------------------------------------- */
