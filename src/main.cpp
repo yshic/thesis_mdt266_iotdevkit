03a7015bdd38820e74c3d389db424973314587d9
Anh Tuan <tuanl799@gmail.com>
2025-04-30 12:47:45 +0700

    update(MAJOR): Integrate SmartHome class into main

--- src/main.cpp
+++ src/main.cpp
@@ -2,83 +2,36 @@
 #include "globals.h"
 
 /* Public defines ----------------------------------------------------- */
-// Main code
-#ifndef DEBUG_I2C
-  #define MAIN
-#endif
 
 /* Forward function declaration --------------------------------------- */
 
-/* Public variables/object --------------------------------------------------- */
+/* Public variables/object -------------------------------------------- */
+SmartHome sh;
+/* Public Class/Struct     -------------------------------------------- */
 
-/* Public Class/Struct     --------------------------------------------------- */
+/* Function definitions ----------------------------------------------- */
 
-/* Function definitions --------------------------------------- */
-
-/* Main code ----------------------------------------------------- */
+/* Main code ---------------------------------------------------------- */
 void setup()
 {
+#ifdef DEBUG_I2C
   Serial.begin(9600);
   Wire.begin();
-  bspGpioPinMode(LED_BUILTIN, OUTPUT);
-  bspGpioDigitalWrite(LED_BUILTIN, HIGH); // TURN OFF LED
-
-#ifdef MAIN
-  #ifndef ENTERPRISE_WIFI
-    #ifdef BLE_MODULE
-  bleSetup();
-    #endif // BLE_MODULE
-  #endif   // ENTERPRISE_WIFI
-
-  #ifdef WIFI_MODULE
-  wifiSetup();
-  #endif
-
-  #ifdef UART_MODULE
-  uartSetup();
-  #endif
-
-  #ifdef DHT20_MODULE
-  dht20Setup();
-  #endif
-
-  #ifdef SHT4X_MODULE
-  sht40Setup();
-  #endif
-
-  #ifdef LIGHT_SENSOR_MODULE
-  lightSensorSetup();
-  #endif
-
-  #ifdef ULTRASONIC_MODULE
-  ultrasonicSetup();
-  #endif
-
-  #ifdef PIR_MODULE
-  pirSensorSetup();
-  #endif
-
-  #ifdef SOIL_MOISTURE_MODULE
-  soilMoistureSetup();
-  #endif
-
-  #ifdef LCD_MODULE
-  lcdSetup();
-  #endif
-
-  #ifdef UNIT_4_RELAY_MODULE
-  unit4RelaySetup();
-  #endif
-
-  #ifdef IOT_SERVER_MODULE
-  iotServerSetup();
-  #endif
-#endif // MAIN
-
-#ifdef DEBUG_I2C
   while (!Serial)
     ; // Wait for Serial to be ready
   scanI2CDevices();
+#else
+  bool devices[11]     = {1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0};
+  bool connectivity[2] = {1, 0};
+
+  sh.deviceEnable(devices);
+  sh.connectivityEnable(connectivity);
+
+  sh.serialSetup(false, true, true);
+  sh.deviceSetup();
+  sh.connectivitySetup();
+
+  sh.iotServerConnect();
 #endif // DEBUG_I2C
 }
 
@@ -86,8 +39,5 @@ void loop()
 {
 #ifdef DEBUG_I2C
   scanI2CDevices();
-#endif
-
-#ifdef MAIN
 #endif
 }
